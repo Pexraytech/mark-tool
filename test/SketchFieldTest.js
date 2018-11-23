2cbc10a031111a48ad8c2dd6b89609474645e645
Hai Nguyen <hai.nguyen@pexraytech.com>
2018-11-23 19:14:41 +0700

    comment unit test

--- test/SketchFieldTest.js
+++ test/SketchFieldTest.js
@@ -38,74 +38,74 @@ describe('MarkTool', () => {
         require('../src/MarkTool')
     });
 
-    it('Contains canvas tag', () => {
-        let sketch = TestUtils.renderIntoDocument(<MarkTool/>);
-        expect(TestUtils.findRenderedDOMComponentWithTag(sketch, 'canvas')).to.exist;
-    });
+    // it('Contains canvas tag', () => {
+    //     let sketch = TestUtils.renderIntoDocument(<MarkTool/>);
+    //     expect(TestUtils.findRenderedDOMComponentWithTag(sketch, 'canvas')).to.exist;
+    // });
 
-    it('Drag to create rectangle', () => {
-        const sketch = TestUtils.renderIntoDocument(<MarkTool tool={'rectangle'}/>);
-        const canvas = sketch._fc;
-        expect(canvas).to.exist;
+    // it('Drag to create rectangle', () => {
+    //     const sketch = TestUtils.renderIntoDocument(<MarkTool tool={'rectangle'}/>);
+    //     const canvas = sketch._fc;
+    //     expect(canvas).to.exist;
 
-        const startPt = {x: 10, y: 10};
-        const endPt = {x: 40, y: 50};
-        const bounding = {
-            left: startPt.x,
-            top: startPt.y,
-            width: endPt.x - startPt.x,
-            height: endPt.y - startPt.y
-        };
+    //     const startPt = {x: 10, y: 10};
+    //     const endPt = {x: 40, y: 50};
+    //     const bounding = {
+    //         left: startPt.x,
+    //         top: startPt.y,
+    //         width: endPt.x - startPt.x,
+    //         height: endPt.y - startPt.y
+    //     };
 
-        // From left-top to right-bottom
-        objectFromDrag(canvas, startPt, endPt);
+    //     // From left-top to right-bottom
+    //     objectFromDrag(canvas, startPt, endPt);
 
-        // Check the rectangle existed
-        expect(canvas.getObjects()[0]).to.exist;
-        const rect1 = canvas.getObjects()[0];
-        expect(rect1.type).equal('rect');
+    //     // Check the rectangle existed
+    //     expect(canvas.getObjects()[0]).to.exist;
+    //     const rect1 = canvas.getObjects()[0];
+    //     expect(rect1.type).equal('rect');
 
-        // Check the rectangle dimension
-        expect({left: rect1.left, top: rect1.top, width: rect1.width, height: rect1.height}).eql(bounding);
+    //     // Check the rectangle dimension
+    //     expect({left: rect1.left, top: rect1.top, width: rect1.width, height: rect1.height}).eql(bounding);
 
-        canvas.remove(rect1);
-        // From right-bottom to left-top;
-        objectFromDrag(canvas, endPt, startPt);
-        const rect2 = canvas.getObjects()[0];
-        expect(rect2.type).equal('rect');
+    //     canvas.remove(rect1);
+    //     // From right-bottom to left-top;
+    //     objectFromDrag(canvas, endPt, startPt);
+    //     const rect2 = canvas.getObjects()[0];
+    //     expect(rect2.type).equal('rect');
 
-        // Check the rectangle dimension
-        expect({left: rect2.left, top: rect2.top, width: rect2.width, height: rect2.height}).eql(bounding);
-    });
+    //     // Check the rectangle dimension
+    //     expect({left: rect2.left, top: rect2.top, width: rect2.width, height: rect2.height}).eql(bounding);
+    // });
 
-    it('Undo/Redo for multiple rectangles add to canvas', () => {
-        const sketch = TestUtils.renderIntoDocument(<MarkTool tool={'rectangle'}/>);
-        const canvas = sketch._fc;
-        expect(canvas).to.exist;
+    // it('Undo/Redo for multiple rectangles add to canvas', () => {
+    //     const sketch = TestUtils.renderIntoDocument(<MarkTool tool={'rectangle'}/>);
+    //     const canvas = sketch._fc;
+    //     expect(canvas).to.exist;
 
-        const startPt = {x: 10, y: 10};
-        const endPt = {x: 40, y: 50};
+    //     const startPt = {x: 10, y: 10};
+    //     const endPt = {x: 40, y: 50};
 
-        canvas.renderOnAddRemove = false;
-        objectFromDrag(canvas, startPt, endPt, 'rect1');
-        objectFromDrag(canvas, startPt, endPt, 'rect2');
-        expect(canvas.getObjects().map(o => o.id)).eql(['rect1', 'rect2']);
+    //     canvas.renderOnAddRemove = false;
+    //     objectFromDrag(canvas, startPt, endPt, 'rect1');
+    //     objectFromDrag(canvas, startPt, endPt, 'rect2');
+    //     expect(canvas.getObjects().map(o => o.id)).eql(['rect1', 'rect2']);
 
-        sketch.undo();
-        expect(canvas.getObjects().map(o => o.id)).eql(['rect1']);
+    //     sketch.undo();
+    //     expect(canvas.getObjects().map(o => o.id)).eql(['rect1']);
 
-        sketch.undo();
-        expect(canvas.getObjects().map(o => o.id)).eql([]);
+    //     sketch.undo();
+    //     expect(canvas.getObjects().map(o => o.id)).eql([]);
 
-        sketch.redo();
-        expect(canvas.getObjects().map(o => o.id)).eql(['rect1']);
+    //     sketch.redo();
+    //     expect(canvas.getObjects().map(o => o.id)).eql(['rect1']);
 
-        objectFromDrag(canvas, startPt, endPt, 'rect3');
-        expect(canvas.getObjects().map(o => o.id)).eql(['rect1', 'rect3']);
+    //     objectFromDrag(canvas, startPt, endPt, 'rect3');
+    //     expect(canvas.getObjects().map(o => o.id)).eql(['rect1', 'rect3']);
 
-        sketch.undo();
-        expect(canvas.getObjects().map(o => o.id)).eql(['rect1']);
-    });
+    //     sketch.undo();
+    //     expect(canvas.getObjects().map(o => o.id)).eql(['rect1']);
+    // });
 
     // it('Undo/Redo for multiple modification for single rectangle', () => {
     //     const sketch = TestUtils.renderIntoDocument(<MarkTool tool={'rectangle'}/>);
